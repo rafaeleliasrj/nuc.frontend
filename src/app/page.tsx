2e3f0b4341a5ddaad2c50d64e3dbba1cf2a4cbf9
Rafael Elias <rafaelelias@outlook.com>
2025-08-17 04:52:07 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,14 +14,14 @@ export default function Home() {
         />
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
+            Comece editando{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
               src/app/page.tsx
             </code>
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+            Salve e veja suas alterações instantaneamente.
           </li>
         </ol>
 
@@ -35,11 +35,11 @@ export default function Home() {
             <Image
               className="dark:invert"
               src="/vercel.svg"
-              alt="Vercel logomark"
+              alt="Logotipo da Vercel"
               width={20}
               height={20}
             />
-            Deploy now
+            Fazer deploy agora
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
@@ -47,7 +47,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            Leia nossa documentação
           </a>
         </div>
       </main>
@@ -61,11 +61,11 @@ export default function Home() {
           <Image
             aria-hidden
             src="/file.svg"
-            alt="File icon"
+            alt="Ícone de arquivo"
             width={16}
             height={16}
           />
-          Learn
+          Aprenda
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
@@ -76,11 +76,11 @@ export default function Home() {
           <Image
             aria-hidden
             src="/window.svg"
-            alt="Window icon"
+            alt="Ícone de janela"
             width={16}
             height={16}
           />
-          Examples
+          Exemplos
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
@@ -91,11 +91,11 @@ export default function Home() {
           <Image
             aria-hidden
             src="/globe.svg"
-            alt="Globe icon"
+            alt="Ícone de globo"
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+          Ir para nextjs.org →
         </a>
       </footer>
     </div>
